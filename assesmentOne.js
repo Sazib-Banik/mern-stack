de435444d2e05641818bf1ce80dd714ffd442b50
Sazib Banik <sazib.banik.1@gmail.com>
2023-03-22 23:39:54 +0600

    assesmentOne done

---
+++ assesmentOne.js
@@ -0,0 +1,10 @@
+function multiplicationTable(num){
+    
+    for (let i=0; i<10; i++){
+        let total = i * num;
+        console.log(total);
+    }
+}
+
+multiplicationTable(2);
+multiplicationTable(4);
